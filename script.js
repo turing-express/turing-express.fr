51c9211997f9018e63dab18a1d4b4d0b9a719dbf
Chantepierre <lucas@lucassifoni.info>
2024-11-18 15:35:19 +0100

    Changes loco

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 (function () {
-    var clamp = function(v, min, max) {
-        return v < min ? min : v > max ? max : v;
+  var clamp = function (v, min, max) {
+    return v < min ? min : v > max ? max : v;
+  };
+  var w = window.innerWidth;
+  var ribbon = document.querySelector(".loco .ribbon");
+  var overflowing = function () {
+    return ribbon.getBoundingClientRect().right > w - 100;
+  };
+  let t = 16;
+  var make = function () {
+    var s = document.createElement("span");
+    if (Math.random() > 0.5) {
+      s.classList.add("zero");
+      s.innerHTML = "0";
+    } else {
+      s.classList.add("one");
+      s.innerHTML = "1";
     }
-    var w = window.innerWidth;
-    var ribbon = document.querySelector('.loco .ribbon');
-    var overflowing = function () {
-        return ribbon.getBoundingClientRect().right > w;
-    };
-    let t = 200;
-    var make = function () {
-        var s = document.createElement('span');
-        if (Math.random() > 0.5) {
-            s.classList.add('zero');
-            s.innerHTML = '0';
-        } else {
-            s.classList.add('one');
-            s.innerHTML = '1';
-        }
-        return s;
-    }
-    var place = function(t) {
-        setTimeout(function() {
-            var el = make();
-            ribbon.appendChild(el);
-            setTimeout(() => {
-                el.classList.add('show');
-            }, 0);
-            if (overflowing()) {
-                ribbon.removeChild(el);
-            } else {
-                var nt = t;
-                place(nt);
-            }
-        }, t);
-    }
-    place(t);
+    return s;
+  };
+  var place = function (t) {
+    setTimeout(function () {
+      var el = make();
+      ribbon.appendChild(el);
+      setTimeout(() => {
+        el.classList.add("show");
+      }, 0);
+      if (overflowing()) {
+        ribbon.removeChild(el);
+      } else {
+        var nt = t;
+        place(nt);
+      }
+    }, t);
+  };
+  place(t);
 })();
